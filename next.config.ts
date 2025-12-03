b7917a622cae2cfff904f5ad9828a3749294dee5
Глеб Степанович <glebstepanovich@Glebs-MacBook-Air.local>
2025-12-03 12:13:18 +0700

    Remove turbopack config from Next config

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,6 @@ const nextConfig: NextConfig = {
   assetPrefix: "/application",
   // Write build output to a custom folder (avoid locked .next)
   distDir: ".wf-next",
-  // Pin Turbopack root to this project to avoid picking up other lockfiles
-  experimental: {
-    turbopack: {
-      root: __dirname,
-    },
-  },
 };
 
 export default nextConfig;
